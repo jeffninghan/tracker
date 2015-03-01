7499116098d71c1e58902c5367671ccd5e6dba77
Jeff Han <jeffninghan@gmail.com>
2015-03-01 15:47:14 -0800

    pytesser file

--- tracker.py
+++ tracker.py
@@ -6,39 +6,6 @@ cap = cv2.VideoCapture(0)
 
 HUE_SCALE_FROM_PAINT_TO_PYTHON = 0.75
 
-# s, img = cam.read()
-# if s:
-#     cv2.namedWindow('cam-test', cv2.CV_WINDOW_AUTOSIZE)
-#     cv2.imshow('cam-test', img)
-#     cv2.waitKey(0)
-#     cv2.destroyWindow('cam-test')
-# def get_filtered_images(cap):
-
-#     # Take each frame
-#     _, frame = cap.read()
-
-#     # Convert BGR to HSV
-#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-#     # define range of blue color in HSV
-#     LOWER_BLUE = [110,50,50]
-#     UPPER_BLUE = [130,255,255]
-#     LOWER_GREEN = [50, 50, 50]
-#     UPPER_GREEN = [110, 255, 255]
-#     lower_color = np.array(LOWER_GREEN, dtype=np.uint8)
-#     upper_color = np.array(UPPER_GREEN, dtype=np.uint8)
-
-#     # Threshold the HSV image to get only blue colors
-#     mask = cv2.inRange(hsv, lower_color, upper_color)
-
-#     # Bitwise-AND mask and original image
-#     res = cv2.bitwise_and(frame,frame, mask= mask)
-#     return res
-
-#     # cv2.imshow('frame',frame)
-#     # cv2.imshow('mask',mask)
-#     # cv2.imshow('res',res)
-    # k = cv2.waitKey(5) & 0xFF
 _, line_frame = cap.read()
 start_time = time.time()
 
@@ -47,8 +14,8 @@ cy = 0
 lastx = 0
 lasty = 0
 start_plot = 0
-while(1):
 
+while(1):
     # Convert BGR to HSV
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -81,17 +48,7 @@ while(1):
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
     cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
     if int(time.time()-start_time) > 5:
         cv2.imshow('line', line_frame)
         cv2.imwrite('line_frames.png', line_frame)
 
-#cv2.destroyAllWindows()
-
-#for i in xrange(0, 10):
-# while True:
-#     res = get_filtered_images(cap)
-#     cv2.imshow('res',res)
-#     time.sleep(1)
