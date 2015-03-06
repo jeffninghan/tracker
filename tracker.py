d6538322af7459fe01d1bcad76fe04b520d9e0e8
kchen92 <chen.kevin92@gmail.com>
2015-03-06 15:40:37 -0800

    rearranged some code and fixed some bugs

--- tracker.py
+++ tracker.py
@@ -51,14 +51,7 @@ def get_moment_center(mm):
 
 # parameters for denoising
 
-def denoise(mask, bitmask_array, start_plot):
-    # parameters
-    num_frames = 15
-    denoising_threshold = 255
-
-    # initialize array
-    if start_plot == 0:
-        bitmask_array = np.empty([num_frames,len(mask),len(mask[0])]) # initialize bitmask_array
+def denoise(mask, bitmask_array, num_frames=2, denoising_threshold = 255):
 
     # shift frames/elements in bitmask_array
     bitmask_array[0:num_frames-1] = bitmask_array[1:num_frames]
@@ -68,7 +61,8 @@ def denoise(mask, bitmask_array, start_plot):
     bitmask_avg = bitmask_array.sum(axis= 0)
     bitmask_avg = bitmask_avg.__div__(num_frames)
     mask = bitmask_avg.__ge__(denoising_threshold)*255
-    mask = np.uint8(bitmask_avg)
+    mask = np.uint8(mask)
+
     return (bitmask_array, mask)
 
 def calibrate():
@@ -83,10 +77,22 @@ def calibrate():
 
     # Threshold the HSV image to get only blue colors
     start_time = time.time()
+    bitmask_array = None
+    start_plot = 0
+
+    ####### only used for initializing bitmask array for denoise #######
+    num_frames = 2
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    bitmask_array = np.empty([num_frames,len(mask),len(mask[0])])
+    ####################################################################
+
     while True:
         _, frame = cap.read()
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower_color, upper_color)
+        bitmask_array, mask = denoise(mask, bitmask_array)
         mm = cv2.moments(mask)
         cv2.imshow('frame',frame)
         if (time.time() - start_time) > 3:
@@ -100,24 +106,33 @@ def run(cx, cy, lastx, lasty, start_plot, threshold):
     reading = False
     start_time = time.time()
     line_frame = None
-    bitmask_array = None
+    num_frames = 2 # for denoising
+    denoising_threshold = 255 # for denoising
+
+    # define range of blue color in HSV
+    LOWER_BLUE = [110,50,50]
+    UPPER_BLUE = [130,255,255]
+    LOWER_GREEN = [50, 50, 50] # LOWER_GREEN = [50, 50, 50]
+    UPPER_GREEN = [100, 255, 255]
+    lower_color = np.array(LOWER_GREEN, dtype=np.uint8)
+    upper_color = np.array(UPPER_GREEN, dtype=np.uint8)
+
+    ####### only used for initializing bitmask array for denoise #######
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    bitmask_array = np.empty([num_frames,len(mask),len(mask[0])])
+    ####################################################################
+
     while True:
         # Convert BGR to HSV
         _, frame = cap.read()
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # define range of blue color in HSV
-        LOWER_BLUE = [110,50,50]
-        UPPER_BLUE = [130,255,255]
-        LOWER_GREEN = [50, 50, 50] # LOWER_GREEN = [50, 50, 50]
-        UPPER_GREEN = [100, 255, 255]
-        lower_color = np.array(LOWER_GREEN, dtype=np.uint8)
-        upper_color = np.array(UPPER_GREEN, dtype=np.uint8)
-
         # Threshold the HSV image to get only blue colors
         mask = cv2.inRange(hsv, lower_color, upper_color)
         
-        bitmask_array, mask = denoise(mask, bitmask_array, start_plot)
+        bitmask_array, mask = denoise(mask, bitmask_array, num_frames, denoising_threshold)
         mm = cv2.moments(mask)
 
         present, value = is_marker_present(mask, mm, threshold)
@@ -145,7 +160,7 @@ def run(cx, cy, lastx, lasty, start_plot, threshold):
             reading = False
             # dont do anything
 
-        time.sleep(0.1)
+        # time.sleep(0.1)
 
         res = cv2.bitwise_and(frame,frame, mask= mask)
 
@@ -161,7 +176,7 @@ def run(cx, cy, lastx, lasty, start_plot, threshold):
         if int(time.time()-start_time) > 10:
             break
 
-        time.sleep(0.1)
+        # time.sleep(0.1)
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
